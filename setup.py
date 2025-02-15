ae4a491312eab51bf20d7b17eee8abd31d8baa2b
akashinGit <chakrabortyakash830@gmail.com>
2025-02-15 23:39:14 +0530

    folder structure added'

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'MediBot Project',
+    version= '0.0.0',
+    author= 'Akash Chakraborty',
+    author_email= 'chakrabortyakash830@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
